c659c085259d4974b3548a384bbbf4aff9ef30b8
Admin <admin@mac-admin.local>
2018-03-25 02:03:00 +0200

    create project

---
+++ MealTime/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  MealTime
+//
+//  Created by zslavman on 25.03.18.
+//  Copyright © 2018 HomeMade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    var array = [Date]()
+    
+    lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }()
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Время когда я ел вкусняшки"
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        let date = array[indexPath.row]
+        
+        cell!.textLabel!.text = dateFormatter.string(from: date)
+        return cell!
+    }
+    
+    
+    
+    
+    @IBAction func addButtonPressed(_ sender: AnyObject) {
+        let date = Date()
+        array.append(date)
+        tableView.reloadData()
+    }
+    
+}
+
+
